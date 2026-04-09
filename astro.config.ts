d242b8db8c59a1b33d191ddbdbeeb0afe351343d
cyx <2513611861@qq.com>
2026-04-09 19:06:19 +0800

    Initial commit with AstroPaper

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,8 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+  site: 'https://patric1013.github.io',
+  base: '/',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
